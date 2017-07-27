5e65e341fca62429ca150378d7eebcb04d4510df
AzurAthena <AzurAthena@gmail.com>
2017-07-27 15:13:03 +0530

    detect faces and eyes

--- detect_faces_and_eyes.py
+++ detect_faces_and_eyes.py
@@ -1,7 +1,7 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
 
 # img = cv2.imread('Files/baby.jpg')
 img = cv2.imread('Files/woman_and_baby.jpg')
